496351e668a91425f115cf6b09540fbc3b863565
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-08-11 17:00:38 -0300

    complete desing

--- src/index.js
+++ src/index.js
@@ -19,7 +19,18 @@ function domText(element, text){
 }
 
 const header = new domCreate('div')
+header.element.setAttribute('id', 'header')
 domAppend(header.element, document.body)
 const tittle = new domCreate('h1')
 domAppend(tittle.element, header.element)
 domText(tittle.element, 'Todo List')
+
+const content = new domCreate('div')
+content.element.setAttribute('id', 'content')
+domAppend(content.element, document.body)
+const menu = new domCreate('div')
+menu.element.setAttribute('id', 'menu')
+domAppend(menu.element, content.element)
+const main = new domCreate('div')
+main.element.setAttribute('id', 'main')
+domAppend(main.element, content.element)
