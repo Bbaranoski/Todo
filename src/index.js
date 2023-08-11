f71e62a5e5e1e64517c3e852eef797cb94af6fb4
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-08-11 01:27:37 -0300

    create dom functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css'
 
-alert('test gai')
+const domCreate = function(element){
+
+    this.element = document.createElement(element)
+
+}
+
+function domAppend(element, local){
+
+    local.appendChild(element)
+
+}
+
+function domText(element, text){
+
+    element.textContent = text
+
+}
+
+const header = new domCreate('div')
+domAppend(header.element, document.body)
+const tittle = new domCreate('h1')
+domAppend(tittle.element, header.element)
+domText(tittle.element, 'Todo List')
