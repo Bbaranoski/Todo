55f7d0c8cd39e10ce6b95752dc76f9a7d8228412
Bbaranoski <breno.b.o.baranoski@gmail.com>
2023-08-29 16:51:26 -0300

    complete to create a list

--- src/dom.js
+++ src/dom.js
@@ -1,82 +1,82 @@
-export function domAppend(element, local){
-
-    local.appendChild(element)
-
-}
-
-export function domText(element, text){
-
-    element.textContent = text
-
-}
-
-export const createList = function(text){
-
-    const list = document.createElement('div')
-    list.classList.add('list')
-    domAppend(list, menu)
-    const tittle = document.createElement('h2')
-    domText(tittle, text)
-    domAppend(tittle, list)
-
-    this.main = document.createElement('div')
-    this.main.classList.add('main')
-    
-    list.addEventListener(('click'), () => {
-
-        const delet = document.getElementById('select')
-        if(this.main != delet){
-            delet.remove()
-            domAppend(this.main, content)
-            this.main.setAttribute('id', 'select')
-        }
-        
-    })
-
-    const buttonListDelet = document.createElement('button')
-    domText(buttonListDelet, 'X')
-    domAppend(buttonListDelet, list)
-
-    buttonListDelet.addEventListener(('click'), e => {
-
-        const delet = document.getElementById('select')
-        delet.remove()
-        e.stopPropagation()
-        list.remove()
-        domAppend(testdiv, content)
-        
-    })
-
-}
-
-const header = document.createElement('div')
-header.setAttribute('id', 'header')
-domAppend(header, document.body)
-const tittle = document.createElement('h1')
-domText(tittle, 'Todo List')
-domAppend(tittle, header)
-
-const content = document.createElement('div')
-content.setAttribute('id', 'content')
-domAppend(content, document.body)
-const menu = document.createElement('div')
-menu.setAttribute('id', 'menu')
-domAppend(menu, content)
-
-const testdiv = document.createElement('div')
-testdiv.setAttribute('id', 'select')
-domAppend(testdiv, content)
-
-const buttonListCreate = document.createElement('div')
-buttonListCreate.setAttribute('id', 'listCreate')
-buttonListCreate.classList.add('list')
-domAppend(buttonListCreate, menu)
-const buttonTittle = document.createElement('h2')
-domText(buttonTittle, 'New List')
-domAppend(buttonTittle, buttonListCreate)
-const listForm = document.createElement('form')
-domAppend(listForm, menu)
-const listInput = document.createElement('input')
-domAppend(listInput, listForm)
-const listSubmmit = document.createElement('button')
-domAppend(listSubmmit, listForm)
+export function domAppend(element, local) {
+    local.appendChild(element);
+  }
+  
+  export function domText(element, text) {
+    element.textContent = text;
+  }
+  
+  export const createList = function (text) {
+    const list = document.createElement("div");
+    list.classList.add("list");
+    domAppend(list, menu);
+    const tittle = document.createElement("h2");
+    domText(tittle, text);
+    domAppend(tittle, list);
+  
+    this.main = document.createElement("div");
+    this.main.classList.add("main");
+  
+    list.addEventListener("click", () => {
+      const delet = document.getElementById("select");
+      if (this.main != delet) {
+        delet.remove();
+        domAppend(this.main, content);
+        this.main.setAttribute("id", "select");
+      }
+    });
+  
+    const buttonListDelet = document.createElement("button");
+    domText(buttonListDelet, "X");
+    domAppend(buttonListDelet, list);
+  
+    buttonListDelet.addEventListener("click", (e) => {
+      const delet = document.getElementById("select");
+      delet.remove();
+      e.stopPropagation();
+      list.remove();
+      domAppend(testdiv, content);
+    });
+  };
+  
+  const header = document.createElement("div");
+  header.setAttribute("id", "header");
+  domAppend(header, document.body);
+  const tittle = document.createElement("h1");
+  domText(tittle, "Todo List");
+  domAppend(tittle, header);
+  
+  const content = document.createElement("div");
+  content.setAttribute("id", "content");
+  domAppend(content, document.body);
+  const menu = document.createElement("div");
+  menu.setAttribute("id", "menu");
+  domAppend(menu, content);
+  
+  const testdiv = document.createElement("div");
+  testdiv.setAttribute("id", "select");
+  domAppend(testdiv, content);
+  
+  const buttonListCreate = document.createElement("div");
+  buttonListCreate.setAttribute("id", "listCreate");
+  buttonListCreate.classList.add("list");
+  domAppend(buttonListCreate, menu);
+  const buttonTittle = document.createElement("h2");
+  domText(buttonTittle, "New List");
+  domAppend(buttonTittle, buttonListCreate);
+  
+  buttonListCreate.addEventListener("click", () => {
+    domAppend(listForm, menu);
+    buttonListCreate.remove();
+  });
+  
+  const listForm = document.getElementById("form");
+  const listTittle = document.getElementById("tittle");
+  listForm.remove();
+  listForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    new createList(listTittle.value);
+    listTittle.value = "";
+    listForm.remove();
+    domAppend(buttonListCreate, menu);
+  });
--- src/todo.js
+++ src/todo.js
@@ -1,37 +1,33 @@
-import {domAppend, domText} from "./dom.js"
+import { domAppend, domText } from "./dom.js";
 
-export const todo = function(tittle, description, dueDate, priority){
+export const todo = function (tittle, description, dueDate, priority) {
+  this.tittle = tittle;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.priority = priority;
+};
 
-    this.tittle = tittle
-    this.description = description
-    this.dueDate = dueDate
-    this.priority = priority
+export function createTodo(List, place) {
+  const div = document.createElement("div");
+  div.classList.add("todo");
+  domAppend(div, place);
 
-}
+  const tittle = document.createElement("h3");
+  domText(tittle, List.tittle);
+  domAppend(tittle, div);
 
-export function createTodo(List, place){
+  const description = document.createElement("p");
+  domText(description, List.description);
+  description.classList.add("description");
+  domAppend(description, div);
 
-    const div = document.createElement('div')
-    div.classList.add('todo')
-    domAppend(div, place)
+  const priority = document.createElement("p");
+  domText(priority, List.priority);
+  priority.classList.add("priority");
+  domAppend(priority, div);
 
-    const tittle = document.createElement('h3')
-    domText(tittle, List.tittle)
-    domAppend(tittle, div)
-
-    const description = document.createElement('p')
-    domText(description, List.description)
-    description.classList.add('description')
-    domAppend(description, div)
-
-    const priority = document.createElement('p')
-    domText(priority, List.priority)
-    priority.classList.add('priority')
-    domAppend(priority, div)
-
-    const date = document.createElement('p')
-    domText(date, List.dueDate)
-    date.classList.add('date')
-    domAppend(date, div)
-
+  const date = document.createElement("p");
+  domText(date, List.dueDate);
+  date.classList.add("date");
+  domAppend(date, div);
 }
